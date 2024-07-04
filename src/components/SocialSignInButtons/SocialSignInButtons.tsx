050bb797b6994e026485563c8ea4b368785e83ad
alejo saez <alejosaezgebicki@gmail.com>
2024-07-04 17:30:11 -0300

    logica para el login de facebook

--- src/components/SocialSignInButtons/SocialSignInButtons.tsx
+++ src/components/SocialSignInButtons/SocialSignInButtons.tsx
@@ -1,10 +1,64 @@
-import CustomButton from '../../components/CustomButton/CustomButton';
 import React from 'react';
 import { GestureResponderEvent } from 'react-native';
+import CustomButton from '../../components/CustomButton/CustomButton';
+import { LoginManager, AccessToken, Profile } from 'react-native-fbsdk-next';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation,NavigationProp } from '@react-navigation/native';
+
+
+type RootStackParamList = {
+  Home: undefined;
+  SignIn: undefined;
+  ForgotPassword: undefined;
+  SignUp: undefined;
+  ConfirmEmail:undefined;
+};
+
+type NavigationProps = NavigationProp<RootStackParamList>;
+
+
 
 const SocialSignInButtons: React.FC = () => {
+  const navigation = useNavigation<NavigationProps>();
+
   const onSingInFacebookPressed = (event: GestureResponderEvent) => {
-    console.warn('Sign In With Facebook');
+    console.log('Sign In With Facebook button pressed');
+    LoginManager.logInWithPermissions(['public_profile', 'email']).then(
+      function (result: any) {
+        if (result.isCancelled) {
+          console.log('Login cancelled');
+        } else {
+          AccessToken.getCurrentAccessToken().then((data: any) => {
+            if (data) {
+              const accessToken = data.accessToken.toString();
+              console.log('Access Token: ', accessToken);
+              fetchUserProfile(accessToken);
+            }
+          });
+        }
+      },
+      function (error: any) {
+        console.log('Login fail with error: ' + error);
+      },
+    );
+  };
+
+  const fetchUserProfile = async (accessToken: string) => {
+    try {
+      const profile = await Profile.getCurrentProfile();
+      if (profile) {
+        const userProfile = {
+          id: profile.userID,
+          name: profile.name,
+          email: profile.email, // Puedes obtener el email a través del API de Facebook si tienes los permisos necesarios
+        };
+        console.log('User Profile:', userProfile);
+        await AsyncStorage.setItem('userProfile', JSON.stringify(userProfile));
+        navigation.navigate('Home'); // Navega a la pantalla principal de tu aplicación
+      }
+    } catch (error) {
+      console.error('Error fetching user profile:', error);
+    }
   };
 
   const onSingInGooglePressed = (event: GestureResponderEvent) => {
